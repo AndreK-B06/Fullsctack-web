2889e3a0f147c0426f94857d32994e2bc195f6ab
André <137167063+AndreK-B06@users.noreply.github.com>
2024-08-19 10:09:31 +0200

    Start working on nav bar

---
+++ src/components/nav/nav-bar.jsx
@@ -0,0 +1,21 @@
+export default function navBar() {
+  return (
+    <>
+      <nav>
+        <div className="game-drop-down nav-btn">
+          <button button className="nav-btn">
+            Game Info
+          </button>
+        </div>
+        <button className="nav-btn">News</button>
+        <button button className="nav-btn">
+          Community
+        </button>
+        <button button className="nav-btn">
+          Sign Up
+        </button>
+        <section className="nav-btn"></section>
+      </nav>
+    </>
+  );
+}
