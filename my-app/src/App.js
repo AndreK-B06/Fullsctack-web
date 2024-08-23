8d28fd3346b421abc1247e276e6ec0f2031db758
Andre <vestrheim-kvist@hotmail.com>
2024-08-23 22:29:32 +0200

    remaike file struksure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <nav></nav>
-      <main></main>
+      <h1>Iceforge Citadel</h1>
+      <section></section>
     </div>
   );
 }
