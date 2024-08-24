60a5097db95fcc5a09b4b084b74a9391f48c173a
Andre <vestrheim-kvist@hotmail.com>
2024-08-24 19:54:51 +0200

    Importing and implementing the navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import NavBar from "./components/nav/nav-bar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Iceforge Citadel</h1>
-      <section></section>
     </div>
   );
 }
