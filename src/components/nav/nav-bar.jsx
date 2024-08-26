ac813ce9c3fbba369870fbb40723abeff9311591
André <137167063+AndreK-B06@users.noreply.github.com>
2024-08-26 17:32:35 +0200

    adding comments temporary

--- src/components/nav/nav-bar.jsx
+++ src/components/nav/nav-bar.jsx
@@ -4,25 +4,27 @@ export default function navBar() {
   return (
     <>
       <nav className="navBar">
-        <div className="game-drop-down nav-btn">
+        {/* Drop down for info like classes races and more. */}
+        <section className="game-drop-down nav-btn">
           <button button className="nav-btn font-btn">
             Game Info
           </button>
-        </div>
+        </section>
 
+        {/* Btn for news, patches and updates */}
         <button className="nav-btn font-btn">News</button>
-
+        {/* Download the game btn */}
         <button button className="nav-btn font-btn">
           Download
         </button>
-
+        {/* section for login and registrations your email */}
         <section className=" flex-row">
           <button button className="nav-btn marg-5 font-btn">
             Sign Up
           </button>
           <button className="nav-btn marg-5 flex-row">
             <img className="icons marg-5" src="/user.png" alt="user img" />
-            <p className="font-btn">Logg in</p>
+            <p className="font-btn">Login</p>
           </button>
         </section>
       </nav>
